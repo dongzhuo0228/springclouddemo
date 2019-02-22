e2be020c1fec7448076eb5c491a4454138348d57
董茁 <dongz001928@yimidida.com>
2019-02-22 16:00:35 +0800

    zuul做服务转发

--- product/src/main/java/com/example/product/ProductApplication.java
+++ product/src/main/java/com/example/product/ProductApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableZuulProxy
 public class ProductApplication {
 
     public static void main(String[] args) {
--- product/src/main/java/com/example/product/ServerClient.java
+++ product/src/main/java/com/example/product/ServerClient.java
@@ -4,7 +4,7 @@ package com.example.product;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value = "eurekatest"/*,fallbackFactory = DeptClientServiceFallbackFactory.class*/)
+@FeignClient(value = "eurekatest",fallbackFactory = DeptClientServiceFallbackFactory.class)
 public interface ServerClient {
     @RequestMapping("/text/msg")
     String test();
